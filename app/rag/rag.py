042655ad3136fa62cdfd94c6f79b8ba028c8510e
het-zignuts <hets@zignuts.com>
2026-02-02 17:04:20 +0530

    Test: Tested conetxt retrieval pipeline

--- app/rag/rag.py
+++ app/rag/rag.py
@@ -1,12 +1,12 @@
 def retrieve_context(vector_db, question: str, min_score: float = 0.3):
-    retriever = vectordb.as_retriever(
+    retriever = vector_db.as_retriever(
         search_type="similarity_score_threshold",
         search_kwargs={
             "k": 4,
             "score_threshold": min_score
         }
     )
-    docs = retriever.get_relevant_documents(question)
+    docs = retriever.invoke(question)
     if not docs:
         return None
     context = "\n\n".join([d.page_content for d in docs])
